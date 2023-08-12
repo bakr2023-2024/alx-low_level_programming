6f3ad86af36924e86e609ef3d7e92944465c490e
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-12 00:47:27 -0700

    .

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+	if (n > 0)
+		printf(" is positive");
+	else if (n == 0)
+		printf(" is zero");
+	else
+		printf(" is negative");
+	printf("\n");
+	return (0);
+}

91948a6773b586a57043670748a43e063e061cf4
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-11 23:23:58 -0700

    .

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * Description:  A column of asterisks on the left side,
+ */
+int main(void)
+{
+	char[] msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(msg,sizeof(msg)-1,1,stderr);
+	return (1);
+}

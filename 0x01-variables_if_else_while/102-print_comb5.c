d4008b56e6350438ce19c666572b81c4f83bb346
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-12 01:54:58 -0700

    .

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (m = 0; m < 100; m++)
+	{
+		for (n = m + 1; n < 100; n++)
+		{
+			putchar(m / 10 - '0');
+			putchar(m % 10 - '0');
+			putchar(' ');
+			putchar(n / 10 - '0');
+			putchar(n % 10 - '0');
+			if (m == 98)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

b76132b984241c3e39c768087e419e2e97821540
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-12 01:32:30 -0700

    .

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,11 @@ int main(void)
 	for (n = 0; n < 10; n++)
 	{
 		putchar(n + '0');
-		if(n == 9)
+		if (n == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }

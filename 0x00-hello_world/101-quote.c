d205c7b64b1bfec38ece9f81de401901585dbcc7
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-11 23:57:13 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,10 +9,10 @@ int main(void)
 	unsigned int i;
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr," ");
 	for (i = 0; i < sizeof(msg); i++)
 	{
 		putchar(msg[i]);
+		fprintf(stderr, " ");
 	}
 	return (1);
 }

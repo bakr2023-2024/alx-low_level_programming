931ccb18cd827542ebbba9f69dc97c844d72abde
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-15 17:09:45 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,4 @@
 #include <stdio.h>
-#include "main.h"
-/**
- * main - Entry point
- * Description: 'the program's description'
- * Return: Always 0 (Success)
-*/
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 /**
  * print_alphabet - prints alphabets in lowercase
  * Return: void

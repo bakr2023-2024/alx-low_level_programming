8daa33c0e9d82720e73029a84160335c9b7d3597
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-15 17:13:02 -0700

    .

---
+++ 0x02-functions_nested_loops/2-print_alphabest_x10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		print_alphabet();
+	}
+}

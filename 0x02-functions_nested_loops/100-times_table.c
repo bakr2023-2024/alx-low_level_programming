e2e788aac4b7e108e665aa718524af2d102b8c4e
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-15 19:58:15 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * times_table - dame tu coista
+ * print_times_table - dame tu coista
+ * @n: max table size
  * Return: void
  */
 
 void print_times_table(int n)
 {
-	int i;
+	int i, j;
 
 	if (n < 0 || n > 15)
 		return;
 	for (i = 0; i <= n; i++)
 	{
-		int j;
-		
 		for (j = 0; j <= n; j++)
 		{
 			int k = i * j;
@@ -43,7 +42,7 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 			_putchar((k % 10) + '0');
-			if(j < n)
+			if (j < n)
 				_putchar(',');
 		}
 		_putchar('\n');

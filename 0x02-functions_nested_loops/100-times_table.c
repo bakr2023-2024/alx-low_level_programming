f923744dfa33037e7b7c04eeb5b7c7d05001cbe2
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-15 19:33:54 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,35 +23,42 @@ void print_times_table(int n)
 			{
 				int t = k / 10;
 
+				if (i > 0 && j < n)
+					_putchar(' ');
 				_putchar((k / 100) + '0');
 				_putchar((t % 10) + '0');
 				_putchar((k % 10) + '0');
 				if (j < n)
 				{
 					_putchar(',');
-					_putchar(' ');
 				}
 			}
 			else if (k >= 10)
 			{
+				if (i > 0 && j < n)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 				if (j < n)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
 				}
 			}
 			else
 			{
+				if (i > 0 && j < n)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+				}
 				_putchar(k + '0');
 				if (j < n)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
 				}
 			}
 		}

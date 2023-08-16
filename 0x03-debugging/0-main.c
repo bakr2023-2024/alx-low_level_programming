0c17190768bcbc74e264ed4c263b9937bd3d017d
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-16 08:49:49 -0700

    .

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}

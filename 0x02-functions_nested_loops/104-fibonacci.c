a8fb7736694c2a2c3539f8e0491274d6966d2330
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-16 08:33:50 -0700

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,15 +6,31 @@
  */
 int main(void)
 {
-	unsigned long int i, a = 1, b = 2, c;
+	unsigned long int i, a = 1, b = 2, c, fib11, fib12, fib21, fib22, h1, h2;
 
 	printf("%lu, %lu, ", a, b);
-	for (i = 3; i <= 98; i++)
+	for (i = 3; i <= 92; i++)
 	{
 		c = a + b;
 		a = b;
 		b = c;
 		printf("%lu", c);
+		printf(", ");
+	}
+	fib11 = a / 10000000000;
+	fib12 = a % 10000000000;
+	fib21 = b / 10000000000;
+	fib22 = b % 10000000000;
+	for (i = 93; i <= 98; i++)
+	{
+		h1 = fib11 + fib12;
+		h2 = fib21 + fib22;
+		if (h2 > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf("%lu%lu", h1, h2);
 		if (i < 98)
 			printf(", ");
 	}

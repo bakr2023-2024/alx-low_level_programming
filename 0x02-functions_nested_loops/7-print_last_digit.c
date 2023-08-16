dcb827e99690c47dd272ebabd07a77b75e5a01d9
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-15 18:06:14 -0700

    .

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - prints and returns last digit
+ * @c: integer to be checked
+ * Return: last digit of c
+ */
+
+int print_last_digit(int c)
+{
+	int ch = c % 10;
+
+	_putchar(ch + '0');
+	return (ch);
+}

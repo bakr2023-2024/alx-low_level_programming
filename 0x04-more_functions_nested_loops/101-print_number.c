3541cc54c22486357e56bb0b90182d219c615728
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-17 08:51:25 -0700

    .

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,30 +7,14 @@
 
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		int i = 0, p, q;
-		unsigned int k = n;
-		do {
-			k /= 10;
-			i++;
-		} while (k != 0);
-		if (n < 0)
-		{
-			k = -n;
-			_putchar('-');
-		}
-		else
-			k = n;
-		for (p = 0; p < i; p++)
-		{
-			int s = k;
+	int k = n;
 
-			for (q = i - p - 1; q > 0; q--)
-				s /= 10;
-			_putchar((s % 10) + '0');
-		}
+	if (n < 0)
+	{
+		_putchar('-');
+		k = -k;
 	}
+	if ((k / 10) > 0)
+		print_number(k / 10);
+	_putchar((k % 10) + '0');
 }

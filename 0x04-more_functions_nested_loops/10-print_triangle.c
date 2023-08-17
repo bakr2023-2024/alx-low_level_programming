8e9afaf00ac08d7e0bcc26069749eb75030514e9
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-17 07:11:47 -0700

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,9 +13,9 @@ void print_triangle(int size)
 	{
 		int i, j;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = n; j > i; j--)
+			for (j = size; j > i; j--)
 				_putchar(' ');
 			for (j = 0; j <= i; j++)
 				_putchar('#');

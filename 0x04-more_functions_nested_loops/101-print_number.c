1baf6c3aabd6b04f3481b7844cbee4e038cc4e9b
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-17 08:03:56 -0700

    .

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - prints a number
+ * @n: the number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int i = 0, k = n, p, q;
+
+	do{
+		k /= 10;
+		i++;
+	}while (k != 0);
+	for(p = 0; p < i; p++)
+	{
+		int s = n;
+		for(q = i - 1; q > 0; q--)
+			s /= 10;
+		_putchar((s % 10) + '0');
+	}
+}

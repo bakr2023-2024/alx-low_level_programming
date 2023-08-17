9f85051f5223594fba064722f9c9ee25d77e13d7
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-17 07:13:02 -0700

    .

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @parameter: 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(0);
+}

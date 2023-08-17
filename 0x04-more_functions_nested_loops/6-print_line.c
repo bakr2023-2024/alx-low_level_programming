283a1f32b99c975439535cba0819e882b24614dd
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-17 06:43:46 -0700

    .

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of dashes
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}

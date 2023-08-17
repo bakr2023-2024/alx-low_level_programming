c83836d423ac0acada018dbf1c679c4be58951a7
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-17 07:10:12 -0700

    .

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - prints trianlge
+ * @size: size of riangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = n; j > i; j--)
+				_putchar(' ');
+			for (j = 0; j <= i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}

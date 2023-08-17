6a4bfdab92e58699d89e63f2c9ab5ace01ecc852
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-17 07:22:42 -0700

    .

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned int n = 612852475143;
+	int i = 2;
+	int max = -1;
+
+	while(n != 1)
+	{
+		if (n % i == 0)
+			n /= i;
+		else
+			i++;
+		if (max < i)
+			max = i;
+	}
+	printf("%d", max);
+	return (0);
+}

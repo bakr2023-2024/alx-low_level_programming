b95f78c3d543f14259a7342cd888698fff9198ad
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-21 09:11:38 -0700

    .

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0, j = 0;
+
+	while (*(str + (i + 1)) != '\0')
+		i++;
+	while (j <= i)
+	{
+		_putchar(*(str + j));
+		j += 2
+	}
+}

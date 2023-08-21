4ad22b20387672340655548bc72763da52cc19c7
bakr2023-2024 <bakr2023.2024@gmail.com>
2023-08-21 08:36:46 -0700

    .

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+		_putc(*(str +i));
+	_putc('\n');
+}

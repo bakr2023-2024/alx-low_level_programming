89f5652788b8f67e824b0e180a700189be3ae0d8
sosomaja <incarnage69@gmail.com>
2023-08-24 23:40:10 +0200

    .

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Entry point
+ * @s: string
+ * Return: uppercase string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0, j = 0;
+
+	while(s[j] != '\0')
+		j++;
+	j--;
+	for(;i < j; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] -= 32;
+	}
+	return (s);
+}

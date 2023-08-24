d15ca8f3b1d144453eb1dede2b140cf563b4d486
sosomaja <incarnage69@gmail.com>
2023-08-24 23:55:22 +0200

    .

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * cap_string - Entry point
+ * @s: string
+ * Return: the capitalizted
+ */
+
+char cap_string(char *s)
+{
+	int i = 0;
+
+	for(; s[i] != '\0'; i++)
+	{
+		if(s[i] == ',' || s[i] == ',' || s[i] == '.' || s[i] == '!')
+			if(s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] -= 32;
+		if(s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')')
+			if(s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] -= 32;
+		if(s[i] == '{' || s[i] == '}')
+			if(s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] -= 32;
+	}
+	return (s);
+}

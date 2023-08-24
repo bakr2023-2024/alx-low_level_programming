d5ac36d6515e57f2f9bb12591c5d2ed02e838737
sosomaja <incarnage69@gmail.com>
2023-08-25 00:47:20 +0200

    .

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,17 +13,8 @@ char *leet(char *s)
 	char *code = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		for (j = 0; alpha[j] != '\0'; j++)
-		{
-			printf("Comparing %c with %c\n",s[i],alpha[j]);
 			if (s[i] == alpha[j])
-			{
-				printf("match\n");
 				s[i] = code[j];
-				printf("after: %c\n",s[i]);
-			}
-		}
-	}
 	return (s);
 }

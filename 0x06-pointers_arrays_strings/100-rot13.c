d6dc8823eeae268c398f5f3ba8e9b8a54a5e18be
sosomaja <incarnage69@gmail.com>
2023-08-25 01:20:42 +0200

    .

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rot13 - Entry point
+ * @s: string
+ * Return: sring afer
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char *og = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *co = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; og[j] != '\0'; j++)
+			if (s[i] == og[j])
+				s[i] = co[j];
+	}
+	return (s);
+}

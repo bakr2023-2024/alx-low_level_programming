32d04558ced275f2cc2a144f6693cf77ba9e4e39
sosomaja <incarnage69@gmail.com>
2023-08-24 23:01:57 +0200

    .

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * @dest: destination
+ * @src: source
+ * @n: bytes
+ * Return: new string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0, k = 0;
+	while (*(src + j) != '\0')
+	{
+		*(dest + i++) = *(src + j++);
+	}
+	for(; j <= n; j++)
+		*(dest + i++) = '\0';
+	return (dest);
+}

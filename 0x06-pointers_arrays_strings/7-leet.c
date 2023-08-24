e8a0d7c76b9e579a8001a4fbd350f68b0c7bdd3b
sosomaja <incarnage69@gmail.com>
2023-08-25 00:24:27 +0200

    .

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * @s: string 
+ * Return: string after
+ */
+
+char *leet(char *s)
+{
+	int i = 0, j = 0;
+	char* alpha = "aAeEoOtTlL";
+	char* code = "4433007711";
+
+	for(; s[i] != '\0'; i++)
+	{
+		for (; alpha[j] != '\0'; j++)
+			if (s[i] == alpha[j])
+				s[i] = code[j];
+	}
+}

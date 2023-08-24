0c13fe516f473cecb98233bda0f6d612ed580677
sosomaja <incarnage69@gmail.com>
2023-08-25 01:58:38 +0200

    .

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	int c = 98;
+	system("a[2] = 98\n");
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);

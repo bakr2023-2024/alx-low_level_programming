de2fd7982fb83d222acdcfceecd1c13cf2432f0f
sosomaja <incarnage69@gmail.com>
2023-08-24 22:28:18 +0200

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,23 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, k, ptr = 0;
+	char *res;
 
 	while (*(dest + i) != '\0')
 		i++;
 	while (*(src + j) != '\0')
 		j++;
-	int k = i + j - 1;
-	char *res = (char *) malloc((k) * sizeof(char));
-	int ptr = 0;
-
+	k = i + j - 1;
+	res = (char *) malloc((k) * sizeof(char));
 	while (ptr < i)
 	{
 		*(res + ptr) = *(dest + ptr);
 		ptr++;
 	}
-	for (ptr = i; ptr <= k; ptr++)
-		*(res + ptr) = *(src + ptr);
-	return (res);
+	ptr = 0;
+	for (; i <= k; ptr++)
+		*(res + (i++)) = *(src + ptr);
+	dest = res;
+	return (dest);
 }

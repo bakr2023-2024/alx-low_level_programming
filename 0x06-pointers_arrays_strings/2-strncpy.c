513387cafa3d9fa8e5cb6ccfa31f81dfe3ab9553
sosomaja <incarnage69@gmail.com>
2023-08-24 23:12:53 +0200

    .

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[i] != '\0' && i < n)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
 		i++;
 	}
 	for (; i < n; i++)
-		*(dest + i++) = '\0';
+		dest[i] = '\0';
 	return (dest);
 }

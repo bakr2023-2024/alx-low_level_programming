32d375bea05b17913e628e8875ebd94809199ddd
sosomaja <incarnage69@gmail.com>
2023-08-24 22:10:49 +0200

    .

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Entry point
+ * @dest: destination
+ * @src: source 
+ * Return: new string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
+		j++;
+	int k = i + j - 1;
+	char* res = (char*) malloc((k) * sizeof(char));
+	int ptr = 0;
+
+	while (ptr < i)
+	{
+		*(res + ptr) = *(dest + ptr);
+		ptr++;
+	}
+	for (ptr = i; ptr <= k; ptr++)
+		*(res + ptr) = *(src + ptr);
+	return res;
+}

2219f2acb9f16d10c402f668885e18b2fc44c5cc
sosomaja <incarnage69@gmail.com>
2023-08-25 03:27:50 +0200

    .

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *  infinite_add- Entry point
+ * @n1: a7a
+ * @n2: a7aaa
+ * @r: a7aaaa
+ * @size_r: niggaaa
+ * Return: char string
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0, carry = 0, result = 0;
+
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	i-=2;
+	j-=2;
+	r[size_r] = '\0';
+	size_r--;
+	for (;size_r >= 0; size_r--)
+	{
+		if (i < 0 && j < 0)
+			break;
+		else if (i < 0)
+			result = 0 + n2[j] - '0' + carry;
+		else if (j < 0)
+			result = 0 + n1[i] - '0' + carry;
+		else
+			result = n1[i] + n2[j] + carry;
+		carry = result / 10;
+		r[size_r] = result % 10;
+	}
+	if(size_r >= 0 && ( i >= 0 || j >= 0))
+		return (NULL);
+	return (r);
+}

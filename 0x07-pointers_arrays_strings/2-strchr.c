f8d90a5057732664ece86c8599314ec6e151ffee
sosomaja <incarnage69@gmail.com>
2023-08-28 23:24:39 +0200

    .

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * @s: string
+ * @c: char
+ * Return: pointer to string
+ */
+
+char *_strchr(char *s, char c)
+{
+	usigned int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+			return (s + i);
+		i++;
+	}
+	return NULL;
+}

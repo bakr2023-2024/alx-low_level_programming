a7e079c27e631c8abd180c0f7823097bfce75360
sosomaja <incarnage69@gmail.com>
2023-08-29 00:48:25 +0200

    .

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: zabola
+ * @needle: ambola
+ * Return: pointer to string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		k = i;
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[k] == needle[j])
+				k++;
+		}
+		if (needle[j] == '\0')
+			return (s + i);
+	}
+	return (NULL);
+}

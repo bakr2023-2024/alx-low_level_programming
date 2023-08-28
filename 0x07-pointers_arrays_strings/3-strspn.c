a1fb1a753998bca5ef1aaf3b1f1cd034f6034384
sosomaja <incarnage69@gmail.com>
2023-08-29 00:17:23 +0200

    .

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: string
+ * @accept: string2
+ * Return: length of segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, len = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				len++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			len = 0;
+	}
+	return (len);
+}

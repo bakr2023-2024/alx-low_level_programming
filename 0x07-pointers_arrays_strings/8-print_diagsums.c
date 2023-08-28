86cd08c808860c874575a12c0cf6a93a9eb319f6
sosomaja <incarnage69@gmail.com>
2023-08-29 01:53:26 +0200

    .

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Entry point
+ * @a: string
+ * @size: size of ambola
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, dig1 = 0, dig2 = 0;
+
+	for (i = 0; i < size ;i++)
+		dig1 += a[i][i];
+	for (i = size - 1; i >= 0; i--)
+		dig2 += a[i][i];
+	printf("%d, %d",dig1, dig2);
+}

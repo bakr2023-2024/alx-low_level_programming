a36066a87c1c970dcb1cdc011cecc68c0506f575
sosomaja <incarnage69@gmail.com>
2023-08-29 02:28:08 +0200

    .

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: string
+ * @to: ambola
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	int i;
+
+	for (i = 0; to[i] != '\0'; i++)
+		*(s + i) = *(t + i);
+	*(s + i) = *(t + i);
+}

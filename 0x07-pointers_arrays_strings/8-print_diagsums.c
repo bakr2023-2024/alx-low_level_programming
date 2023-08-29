75f1c876a7cce01b4d2a98d2ea56acb21b026eea
sosomaja <incarnage69@gmail.com>
2023-08-29 02:21:39 +0200

    .

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,10 +10,10 @@
 void print_diagsums(int *a, int size)
 {
 	int i, dig1 = 0, dig2 = 0;
-	for (i = 0; i < size * size; i+=size + 1)
+	for (i = 0; i < size * size; i+= size + 1)
 		dig1 += *(a + i);
 	for (i = size - 1; i <= size * size - size; i += size - 1)
 		dig2 += *(a + i);
-	printf("%d, %d\n",dig1, dig2);
+	printf("%d, %d\n", dig1, dig2);
 
 }

2dea63a89edae6ea419db3e0aacd888908a07ee1
sosomaja <incarnage69@gmail.com>
2023-08-31 00:31:54 +0200

    .

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,29 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Entry point
+ * helper - Entry point
  * @n: number
+ * @inc: num2
  * Return: numb
  */
-
+int helper(int n, int inc)
+{
+	if (n - inc == 0)
+		return (1);
+	else if (n - inc < 0)
+		return (-1);
+	return (1 + helper(n - inc, inc + 2));
+}
+/**
+ * _sqrt_recursion - numb
+ * @n: numae
+ * Return: numa
+ */
 int _sqrt_recursion(int n)
 {
-
+	int div;
+	div = helper(n, 1);
+	if (n % div != 0 || div * div != n)
+		return (-1);
+	return (div);
 }

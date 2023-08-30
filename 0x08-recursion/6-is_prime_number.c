e6933ec5c29cb048df9754f185f18375d3761899
sosomaja <incarnage69@gmail.com>
2023-08-31 00:54:42 +0200

    .

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,14 +11,14 @@ int helper(int n, int inc)
 		return (0);
 	if (inc == n)
 		return (n);
-	else if(n % inc == 0)
+	else if (n % inc == 0)
 		return (0);
-	return (helper(n, inc + 1);
+	return (helper(n, inc + 1));
 
 }
 /**
  * is_prime_number - Entry point
- * @n: number 
+ * @n: number
  * Return: num
  */
 int is_prime_number(int n)

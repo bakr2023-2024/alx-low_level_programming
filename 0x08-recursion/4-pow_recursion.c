7ba31b27c9a1bed76d6e011e46c16fe958ff214d
sosomaja <incarnage69@gmail.com>
2023-08-30 23:32:23 +0200

    .

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ * @x: num1
+ * @y: num2
+ * Return: POWWEEEER
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	else
+		return x * _pow_recursion(y - 1);
+}

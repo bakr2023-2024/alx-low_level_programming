7b09014cf02b02e3b6a188f7127dcbe584b8c93f
sosomaja <incarnage69@gmail.com>
2023-08-31 00:54:18 +0200

    .

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * helper - Entry point
+ * @n: num
+ * @inc: num2
+ * Return: integer
+ */
+int helper(int n, int inc)
+{
+	if (n < 1)
+		return (0);
+	if (inc == n)
+		return (n);
+	else if(n % inc == 0)
+		return (0);
+	return (helper(n, inc + 1);
+
+}
+/**
+ * is_prime_number - Entry point
+ * @n: number 
+ * Return: num
+ */
+int is_prime_number(int n)
+{
+	return ((helper(n, 1) == n) ? 1 : 0);
+}

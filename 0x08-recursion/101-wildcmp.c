cc64bd047c433f69035cbfe9f9e80b589fa38489
sosomaja <incarnage69@gmail.com>
2023-08-31 03:42:19 +0200

    .

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,69 +1,27 @@
 #include "main.h"
-#include <stdio.h>
-/**
- * _strlen - Entry point
- * @s: string
- * Return: number
- */
-int _strlen(char *s)
-{
-	if (s[0] == '\0')
-		return (0);
-	return (1 + _strlen(s + 1));
-}
-/**
- * helper - Entry point
- * @s1: string1
- * @s2: string2
- * @idx1: index1
- * @idx2: index2
- * Return: Always number
- */
-int helper(char *s1, char *s2, int idx1, int idx2)
-{
-	if (s1[idx1] == '\0' && s2[idx2] != '\0')
-	{
-		return (0);
-	}
-	else if(s1[idx1] != '\0' && s2[idx2] == '\0')
-		return (0);
-	else if(s1[idx1] == '\0')
-		return (1);
-	if (s2[idx2] == '*')
-	{
-		if (s1[idx1] == s2[idx2 + 1])
-		{
-			idx2++;
-		}
-		else
-		{
-			idx1++;
-		}
-	}
-	else
-	{
-		if (s1[idx1] == s2[idx2])
-		{
-			idx1++;
-			idx2++;
-		}
-		else
-		{
-			return (0);
-		}
-	}
-	return (helper(s1, s2, idx1, idx2));
-}
+
 /**
  * wildcmp - Entry point
  * @s1: string1
  * @s2: string2
  * Return: ambola
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	int help;
+	if (s1[0] == '\0' && s2[0] == '\0')
+		return (1);
 
-	help = helper(s1, s2, 0, 0);
-	return (help);
+	if (s1[0] == s2[0])
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (s2[0] == '*')
+	{
+		if (s1[0] == '\0' && s2[1] != '\0')
+			return (0);
+		else if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
+	return (0);
 }

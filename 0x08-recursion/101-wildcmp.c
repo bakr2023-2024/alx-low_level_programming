75cb735b569210cefcd7a58be09142a062327e68
sosomaja <incarnage69@gmail.com>
2023-08-31 02:59:38 +0200

    .

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -27,7 +27,7 @@ int helper(char *s1, char *s2, int idx1, int idx2)
 	}
 	if (s2[idx2] == '*')
 	{
-		if (s2[idx2 + 1] != '\0' && s1[idx1] == s2[idx2 + 1])
+		if (s1[idx1] == s2[idx2 + 1])
 		{
 			idx2++;
 		}

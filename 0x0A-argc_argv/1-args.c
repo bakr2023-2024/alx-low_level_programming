26fe444a2dab1c63778bfbde7b8f9ebf5e667f3e
sosomaja <incarnage69@gmail.com>
2023-09-01 11:50:57 +0200

    .

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,8 +10,10 @@
 int main(int argc, char *argv[])
 {
 	char **ag = argv;
+	unsigned long int a = sizeof(ag);
 
 	ag = NULL;
-	printf("%ld\n", argc - sizeof(ag) + sizeof(ag));
+	a = 0;
+	printf("%d\n", argc);
 	return (0);
 }

1514213da693ea1c057cf591b575498615ac3b5c
sosomaja <incarnage69@gmail.com>
2023-09-01 11:43:32 +0200

    .

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: num
+ * @argv: string
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

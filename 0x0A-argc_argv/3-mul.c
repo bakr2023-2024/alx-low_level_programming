0688c743fc8a8bd7c843c23b9942f90d6e3cab78
sosomaja <incarnage69@gmail.com>
2023-09-01 12:01:27 +0200

    .

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: buena
+ * @argv: kuena
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	else
+	{
+		int a = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", a);
+	}
+	return (0);
+}

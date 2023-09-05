b6615f688591a580da19090e95dafd7ed6e4e94a
sosomaja <incarnage69@gmail.com>
2023-09-05 13:51:54 +0200

    .

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Entry point
+ * @width: widthh
+ * @height: heighth
+ * Return: array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int **rows;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	rows = malloc(sizeof(int *) * height);
+	if (rows == NULL)
+		return (NULL);
+	for (i = 0; i < width; i++)
+	{
+		*(rows + i) = malloc(sizeof(int) * width);
+		if (*(rows + i) == NULL)
+			return (NULL);
+	}
+	return (rows);
+}

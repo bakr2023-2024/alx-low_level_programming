8db330d2b7f1650976f09d6fafaca03d53e409ec
sosomaja <incarnage69@gmail.com>
2023-09-05 13:27:06 +0200

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	char *arr;
-	arr = malloc(sizeof(char) * size);
 
+	arr = malloc(sizeof(char) * size);
 	if (arr == NULL)
 		return (NULL);
 	*(arr) = c;

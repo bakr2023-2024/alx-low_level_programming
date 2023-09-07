292c0193f4074f59d6f9a1c6f04a2e2ddb1e824c
sosomaja <incarnage69@gmail.com>
2023-09-07 20:50:26 +0200

    .

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _realloc - Entry point
+ * @ptr: shabola
+ * @old_size: zabola
+ * @new_size: ambola
+ * Return: bibi kaka
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i, j;
+	void *ptr2;
+
+	if (ptr == NULL)
+	{
+		ptr2 = malloc(new_size);
+		return (ptr2);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr2 = malloc(new_size);
+	for (i = 0; i < old_size; i++)
+		ptr2[i] = ptr[i];
+	free(ptr);
+	return (ptr2);
+}

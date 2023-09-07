9fa3e3030af2a0a1b4a3601f273fcef9f3a9a1a7
sosomaja <incarnage69@gmail.com>
2023-09-07 15:12:59 +0200

    .

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _calloc - Entry point
+ * @nmemb: num
+ * @size: bunver
+ * Return: void ambola
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *point;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	point = malloc(nmemb * size);
+	if (point == NULL)
+		return (NULL);
+	return (point);
+}

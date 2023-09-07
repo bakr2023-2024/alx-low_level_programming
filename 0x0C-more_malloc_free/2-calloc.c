144a7fdbe45129d15c90185f5eeb8d37cdaba6b5
sosomaja <incarnage69@gmail.com>
2023-09-07 15:21:39 +0200

    .

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *point;
+	int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	point = malloc(nmemb * size);
 	if (point == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		point[i] = 0;
 	return (point);
 }

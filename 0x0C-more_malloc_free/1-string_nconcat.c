59b79170e49821e919b28ffdb8bc5ad9f1daf6a4
sosomaja <incarnage69@gmail.com>
2023-09-07 14:20:20 +0200

    .

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _strlen - ambola
+ * @s: string
+ * Return: number
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i - 1);
+}
+/**
+ * string_nconcat - Entry point
+ * @s1: str
+ * @s2: ss
+ * @n: num
+ * Return: ambola
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, p, q;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = _strlen(s1);
+	j = _strlen(s2);
+	str = malloc(i + j);
+	if (str == NULL)
+		return (NULL);
+	for (p = 0; p < i; p++)
+		str[p] = s1[p];
+	for (q = 0; q < j && j < n; j++)
+		str[p++] = s2[q];
+	str[p] = '\0';
+	return (str);
+}

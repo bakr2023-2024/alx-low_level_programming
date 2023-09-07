c15a82c9a8ccd7eaa93df634c111a7e627eaedb9
sosomaja <incarnage69@gmail.com>
2023-09-07 14:31:41 +0200

    .

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	i = _strlen(s1);
 	j = _strlen(s2);
 	printf("Size: %u, %u\n", i, j);
-	str = malloc(i + j + 1);
+	str = malloc(i + j - 1);
 	if (str == NULL)
 		return (NULL);
 	for (p = 0; p < i; p++)

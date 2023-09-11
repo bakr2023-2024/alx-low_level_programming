8ba68b4ffd5db80223b35de1d0ad29c181fa3bad
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-09-11 22:41:29 +0300

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,5 @@ struct dog
 typedef struct dog dog_t;
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif

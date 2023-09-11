6f1a2b9e1cee0e2f3eff0e1480c5f1b6b21622a6
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-09-11 22:33:33 +0300

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,16 @@
  * @age: agee
  * @owner: calabunga
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
+/**
+ * dog_t - kaka bibi
+ */
+typedef struct dog dog_t;
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
 #endif

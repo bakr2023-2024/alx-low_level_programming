5eb6fb8d42c30699b2a67e1241d426fabca03727
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-09-13 18:58:50 +0300

    .

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - Entry point
+ * @array: ambola
+ * @size: zabola
+ * @cmp: kabola
+ * Return: dame ti cosita
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, result = 0;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (; i < size; i++)
+	{
+		result = cmp(array[i]);
+		if (result != 0)
+			return (result);
+	}
+	return (-1);
+}

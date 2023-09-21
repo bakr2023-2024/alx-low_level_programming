3173fdcde7a1ece4a78495318919108ec899ab19
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-09-22 02:12:00 +0300

    .

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 void before_main(void) __attribute__((constructor));
 /**
  * before_main - thank you
  */
 void before_main(void)
 {
-	char *s1 = "You're beat! and yet you must allow,\n";
-	char *s2 = "I bore my house upon my back!\n";
+	char *s1 = strdup("You're beat! and yet you must allow,\n");
+	char *s2 = strdup("I bore my house upon my back!\n");
 
 	printf("%s%s", s1, s2);
+	free(s1);
+	free(s2);
 }

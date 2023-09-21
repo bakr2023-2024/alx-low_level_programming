5a6d646c26b19c132413b2123fad75854523ab7f
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-09-22 01:37:09 +0300

    .

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_node_end - entry point
+ * @head: kaka
+ * @str: bibi
+ * Return: ambola
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(*head));
+	list_t *ptr = *head;
+
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = new;
+	return (new);
+}

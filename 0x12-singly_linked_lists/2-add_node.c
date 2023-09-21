32b012c151b1fb4504a1529afed5251b55c922e0
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-09-22 01:31:38 +0300

    .

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,12 +7,12 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *ptr = malloc(sizeof(head));
+	list_t *ptr = malloc(sizeof(*head));
 	
 	if (ptr == NULL)
 		return (NULL);
 	ptr->str = strdup(str);
 	ptr->len = strlen(str);
-	ptr->next = head;
+	ptr->next = *head;
 	return (ptr);
 }

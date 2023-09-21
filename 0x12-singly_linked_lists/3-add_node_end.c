626929b66b040a9f28159e5df22727496ea9c177
bakr2023-2024 <140170067+bakr2023-2024@users.noreply.github.com>
2023-09-22 01:37:48 +0300

    .

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = strlen(str);
-	while (ptr != NULL)
+	while (ptr->next != NULL)
 	{
 		ptr = ptr->next;
 	}
